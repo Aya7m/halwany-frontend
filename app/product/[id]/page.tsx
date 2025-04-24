ae588c61807008992e391a959f48de341ef8954e
Aya7m <yyoyo6987@gmail.com>
2025-04-24 17:40:29 +0200

    hello

--- app/product/[id]/page.tsx
+++ app/product/[id]/page.tsx
@@ -1,8 +1,7 @@
-
-// app/product/[id]/page.tsx
 import axios from "axios";
 import AddToCartButton from "./AddToCartButton";
 import Image from "next/image";
+import { notFound } from "next/navigation";
 
 interface Product {
   _id: string;
@@ -13,40 +12,39 @@ interface Product {
   category: string;
 }
 
-interface PageProps  {
+interface Params {
   params: {
     id: string;
   };
 }
 
-const ProductDetailsPage = async ({ params }: PageProps ) => {
-  const response = await axios.get<Product>(
-    `https://halwany-backend-production.up.railway.app/product/${params.id}`
-  );
+const ProductDetailsPage = async ({ params }: Params) => {
+  try {
+    const response = await axios.get<Product>(
+      `https://halwany-backend-production.up.railway.app/product/${params.id}`
+    );
+    const product = response.data;
 
-  const product = response.data;
-
-  return (
-    <div className="max-w-2xl mx-auto mt-12">
-      <Image
-        src={product.image}
-        alt={product.name}
-        className="w-full h-80 object-cover rounded-lg mb-4"
-      />
-      <h1 className="text-3xl font-bold mb-2">{product.name}</h1>
-      <p className="text-lg text-gray-700 mb-4">{product.description}</p>
-      <p className="text-2xl font-semibold text-green-600">${product.price}</p>
-      <div className="flex w-full items-center justify-center">
-        <AddToCartButton productId={product._id}/>
+    return (
+      <div className="max-w-2xl mx-auto mt-12">
+        <Image
+          src={product.image}
+          alt={product.name}
+          width={500}
+          height={300}
+          className="w-full h-80 object-cover rounded-lg mb-4"
+        />
+        <h1 className="text-3xl font-bold mb-2">{product.name}</h1>
+        <p className="text-lg text-gray-700 mb-4">{product.description}</p>
+        <p className="text-2xl font-semibold text-green-600">${product.price}</p>
+        <div className="flex w-full items-center justify-center">
+          <AddToCartButton productId={product._id} />
+        </div>
       </div>
-    </div>
-  );
+    );
+  } catch (error) {
+    notFound();
+  }
 };
 
 export default ProductDetailsPage;
-
-
-
-
-
-
