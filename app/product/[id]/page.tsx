de9699b841e580a3f134cb8f627dbd0ab504d1d6
Aya7m <yyoyo6987@gmail.com>
2025-04-24 08:33:43 +0200

    editPageDetails

--- app/product/[id]/page.tsx
+++ app/product/[id]/page.tsx
@@ -1,6 +1,51 @@
 
+// // app/product/[id]/page.tsx
+// import axios from "axios";
+// import AddToCartButton from "./AddToCartButton";
+
+// interface Product {
+//   _id: string;
+//   name: string;
+//   description: string;
+//   price: number;
+//   image: string;
+//   category: string;
+// }
+
+// interface PageProps  {
+//   params: {
+//     id: string;
+//   };
+// }
+
+// const ProductDetailsPage = async ({ params }: PageProps ) => {
+//   const response = await axios.get<Product>(
+//     `https://halwany-backend-production.up.railway.app/product/${params.id}`
+//   );
+
+//   const product = response.data;
+
+//   return (
+//     <div className="max-w-2xl mx-auto mt-12">
+//       <img
+//         src={product.image}
+//         alt={product.name}
+//         className="w-full h-80 object-cover rounded-lg mb-4"
+//       />
+//       <h1 className="text-3xl font-bold mb-2">{product.name}</h1>
+//       <p className="text-lg text-gray-700 mb-4">{product.description}</p>
+//       <p className="text-2xl font-semibold text-green-600">${product.price}</p>
+//       <div className="flex w-full items-center justify-center">
+//         <AddToCartButton productId={product._id}/>
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default ProductDetailsPage;
+
+
 // app/product/[id]/page.tsx
-import axios from "axios";
 import AddToCartButton from "./AddToCartButton";
 
 interface Product {
@@ -19,11 +64,16 @@ interface PageProps  {
 }
 
 const ProductDetailsPage = async ({ params }: PageProps ) => {
-  const response = await axios.get<Product>(
-    `https://halwany-backend-production.up.railway.app/product/${params.id}`
-  );
+  const res = await fetch(`https://halwany-backend-production.up.railway.app/product/${params.id}`, {
+    cache: "no-store"
+  });
 
-  const product = response.data;
+  if (!res.ok) {
+    // ممكن كمان ترجعي NotFound component
+    throw new Error("Failed to fetch product");
+  }
+
+  const product: Product = await res.json();
 
   return (
     <div className="max-w-2xl mx-auto mt-12">
@@ -36,7 +86,7 @@ const ProductDetailsPage = async ({ params }: PageProps ) => {
       <p className="text-lg text-gray-700 mb-4">{product.description}</p>
       <p className="text-2xl font-semibold text-green-600">${product.price}</p>
       <div className="flex w-full items-center justify-center">
-        <AddToCartButton productId={product._id}/>
+        <AddToCartButton productId={product._id} />
       </div>
     </div>
   );
