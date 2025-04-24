9888f04dbd122e12b8bc2b239824568bb5c76e78
Aya7m <yyoyo6987@gmail.com>
2025-04-24 07:45:16 +0200

    hanleLoading

---
+++ app/components/LoadingSpinner.tsx
@@ -0,0 +1,7 @@
+// app/components/LoadingSpinner.tsx
+const LoadingSpinner = ({ size }: { size?: string }) => {
+    return <div className={`spinner ${size}`}></div>;
+  };
+  
+  export default LoadingSpinner;
+  
